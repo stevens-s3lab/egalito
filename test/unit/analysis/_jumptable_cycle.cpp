8f94390ea06ee90c499705013a42bec41abb4b7b
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2026-05-19 14:14:47 +0200

    add test cases

--- test/unit/analysis/_jumptable_cycle.cpp
+++ test/unit/analysis/_jumptable_cycle.cpp
@@ -5,17 +5,29 @@
 #include "analysis/jumptabledetection.h"
 #undef private
 
-TEST_CASE("skip cyclic jump table base address lookup", "[analysis][fast]") {
+TEST_CASE("handle base address cycle", "[analysis][fast]") {
+#ifdef ARCH_X86_64
+    int reg = X86_REG_R9;
+    auto memory = TreeFactory::instance().make<TreeNodeAddition>(
+        TreeFactory::instance().make<TreeNodePhysicalRegister>(reg, 8),
+        TreeFactory::instance().make<TreeNodeConstant>(-88));
+
+    RegMemState state(nullptr, nullptr);
+    state.addRegRef(reg, &state);
+    state.addRegDef(reg,
+        TreeFactory::instance().make<TreeNodeDereference>(memory, 8));
+    state.addMemRef(reg, &state);
+    state.addMemDef(reg,
+        TreeFactory::instance().make<TreeNodeAddition>(
+            TreeFactory::instance().make<TreeNodePhysicalRegister>(reg, 8),
+            TreeFactory::instance().make<TreeNodeConstant>(-88)));
+
     JumptableDetection jt(nullptr);
-    auto state = reinterpret_cast<UDState *>(0x1);
-    int reg = 9;
-
-    jt.activeBaseAddressLookups.insert(std::make_pair(state, reg));
-
     bool found;
     address_t addr;
-    std::tie(found, addr) = jt.parseBaseAddress(state, reg);
+    std::tie(found, addr) = jt.parseBaseAddress(&state, reg);
 
     REQUIRE(!found);
     REQUIRE(addr == 0);
+#endif
 }
